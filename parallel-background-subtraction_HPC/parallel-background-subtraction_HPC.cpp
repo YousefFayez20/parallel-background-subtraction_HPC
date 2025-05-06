9abbf718b7fbcf0ab3ec256baddd816b5293a306
XXMariamX <rura.essam@hotmail.com>
2025-05-06 17:37:38 +0300

    added some code organization

--- parallel-background-subtraction_HPC/parallel-background-subtraction_HPC.cpp
+++ parallel-background-subtraction_HPC/parallel-background-subtraction_HPC.cpp
@@ -7,6 +7,7 @@ using namespace std;
 using namespace cv;
 namespace fs = std::filesystem;
 
+// === Helper Functions === //
 
 string clean_path(string path) {
     
@@ -46,6 +47,128 @@ void get_dataset_names(vector<pair<string, string>> & dataset_name_path) {
             }            
         }
     }
+}
+
+// === Algorithm Steps === //
+
+bool set_directories(string& input_directory, string& output_directory) {
+    
+        string input;
+        // == Dataset Names == //
+        vector<pair<string,string>> dataset_name_path;
+        get_dataset_names(dataset_name_path);
+
+        cout << "Please Choose A Dataset For Your Inputes Frames Directory: \n";
+        for(int i = 0; i < dataset_name_path.size(); i++){
+            cout << i + 1 << ") "<< dataset_name_path[i].first << "\n";
+        }
+        
+        // == Input Directory == //
+          cout << "Or Enter The Path Of Your Inputes Frames Directory: \n";
+
+          getline(cin, input);
+          input = clean_path(input);
+
+          if(is_number(input)) {
+              // Validating dataset number.
+              int number = stoi(input) - 1;
+              if (number < 0 || number >= dataset_name_path.size()) {
+                  cerr << "Error: Input number does not exist or is not a valid number.\n";
+                  return 0;
+              }
+              input_directory  = dataset_name_path[stoi(input) - 1].second;
+              output_directory = "Output\\" + dataset_name_path[stoi(input) - 1].first;
+          } else
+              input_directory = input;
+      
+
+  // Validating selected input directory.
+  if (!fs::exists(input_directory) || !fs::is_directory(input_directory)) {
+      cerr << "Error: Input directory does not exist or is not a valid folder.\n";
+      return 0;
+  }
+
+
+        // == Output Directory == //
+  if(output_directory.empty()){
+      cout << "Please Enter The Desired output Path.: \n";
+      getline(cin, output_directory);
+      output_directory = clean_path(output_directory);
+  }
+ 
+
+
+
+
+  // Validating selected output directory (+ Creating it if it doesn't exist.) 
+  if (!fs::exists(output_directory)) {
+      if (!fs::create_directory(output_directory)) {
+          cerr << "Error: Could not create output directory!" << endl;
+          return 0;
+      }
+  }
+  return 1; // no error
+}
+
+bool load_images(vector<pair<string, Mat>>& input_images, const string& input_directory) {
+    try {
+        for (const auto& entry : fs::directory_iterator(input_directory)) {
+            if (entry.is_regular_file()) {
+                Mat img = imread(entry.path().string(), IMREAD_GRAYSCALE);
+                if (img.empty()) {
+                    cerr << "Warning: Could not read image: " << entry.path() << endl;
+                    continue;
+                }
+                input_images.push_back({ entry.path().filename().string(), img });
+                cout << "Loaded: " << entry.path() << endl;
+            }
+        }
+    }
+    catch (const fs::filesystem_error& e) {
+        cerr << "Filesystem error: " << e.what() << endl;
+        return 0;
+    }
+
+    if (input_images.empty()) {
+        cerr << "Error: No images found in directory." << endl;
+        return 0;
+    }
+    return 1; // no error
+}
+
+bool set_frames(int& nFrames_selected, const int& input_images_size){
+    cout << "How Many Frames would you like to process? (Min: 1, Max: " << input_images_size << "): ";
+	cin >> nFrames_selected;
+
+    if (nFrames_selected <= 0 || nFrames_selected > input_images_size)
+    {
+		cerr << "Error: Invalid number of frames selected. Exiting." << endl;
+        return 0;
+    }
+    return 1;
+}
+
+bool validate_images_same_size(const vector<Mat>& images) {
+    if (images.empty()) {
+        cerr << "Error: No images to validate!" << endl;
+        return false;
+    }
+
+    int standard_rows = images[0].rows;
+    int standard_cols = images[0].cols;
+
+    for (size_t i = 1; i < images.size(); ++i) {
+        if (images[i].rows != standard_rows || images[i].cols != standard_cols) {
+            cerr << "Error: Image at index " << i << " does not match size ("
+                << standard_rows << "x" << standard_cols << "). "
+                << "Found (" << images[i].rows << "x" << images[i].cols << ")." << endl;
+            return false;
+        }
+    }
+
+    cout << "All images have consistent size: "
+        << standard_rows << "x" << standard_cols << "." << endl;
+    return true;
 }
 
 Mat compute_estimated_background(const vector<Mat>& images) {
@@ -113,27 +236,24 @@ Mat compute_estimated_background(const vector<Mat>& images) {
     return background_final;
 }
 
-bool validate_images_same_size(const vector<Mat>& images) {
-    if (images.empty()) {
-        cerr << "Error: No images to validate!" << endl;
-        return false;
-    }
-
-    int standard_rows = images[0].rows;
-    int standard_cols = images[0].cols;
-
-    for (size_t i = 1; i < images.size(); ++i) {
-        if (images[i].rows != standard_rows || images[i].cols != standard_cols) {
-            cerr << "Error: Image at index " << i << " does not match size ("
-                << standard_rows << "x" << standard_cols << "). "
-                << "Found (" << images[i].rows << "x" << images[i].cols << ")." << endl;
-            return false;
+bool extract_images_for_processing(vector<Mat>& images_only, const vector<pair<string, Mat>>& input_images, const int& nFrames_selected){
+    try {
+        for (int i = 0; i < nFrames_selected; i++) {
+            images_only.push_back(input_images[i].second);
         }
     }
+    catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 0;
+    }
+    return 1;
 
-    cout << "All images have consistent size: "
-        << standard_rows << "x" << standard_cols << "." << endl;
-    return true;
+}
+
+void save_display_background(const string& output_directory, const Mat& background){
+    imwrite(output_directory + "\\estimated_background.jpg", background);
+    imshow("Estimated Background", background);
+    waitKey(500); // Show for a short time
 }
 
 Mat compute_foreground_mask(const Mat& background, const Mat& frame, int threshold_value) {
@@ -180,158 +300,7 @@ Mat compute_foreground_mask(const Mat& background, const Mat& frame, int thresho
     return foreground_mask;
 }
 
-
-
-int main() {
-
-
-    // === Receiving Directories === //
-
-          string input, input_directory, output_directory;
-          // == Dataset Names == //
-          vector<pair<string,string>> dataset_name_path;
-          get_dataset_names(dataset_name_path);
-  
-          cout << "Please Choose A Dataset For Your Inputes Frames Directory: \n";
-          for(int i = 0; i < dataset_name_path.size(); i++){
-              cout << i + 1 << ") "<< dataset_name_path[i].first << "\n";
-          }
-          
-	      // == Input Directory == //
-            cout << "Or Enter The Path Of Your Inputes Frames Directory: \n";
-
-            getline(cin, input);
-            input = clean_path(input);
-
-            if(is_number(input)) {
-                // Validating dataset number.
-                int number = stoi(input) - 1;
-                if (number < 0 || number >= dataset_name_path.size()) {
-                    cerr << "Error: Input number does not exist or is not a valid number.\n";
-                    return -1;
-                }
-                input_directory  = dataset_name_path[stoi(input) - 1].second;
-                output_directory = "Output\\" + dataset_name_path[stoi(input) - 1].first;
-            } else
-                input_directory = input;
-        
-
-    // Validating selected input directory.
-    if (!fs::exists(input_directory) || !fs::is_directory(input_directory)) {
-        cerr << "Error: Input directory does not exist or is not a valid folder.\n";
-        return -1;
-    }
-
-
-          // == Output Directory == //
-    if(output_directory.empty()){
-        cout << "Please Enter The Desired output Path.: \n";
-        getline(cin, output_directory);
-        output_directory = clean_path(output_directory);
-    }
-   
-
-
-
-
-    // Validating selected output directory (+ Creating it if it doesn't exist.) 
-    if (!fs::exists(output_directory)) {
-        if (!fs::create_directory(output_directory)) {
-            cerr << "Error: Could not create output directory!" << endl;
-            return -1;
-        }
-    }
-          // == Output Directory == //
-
-
-
-
-
-    vector<pair<string, Mat>> input_images;
-
-    // === Loading Images ===
-    try {
-        for (const auto& entry : fs::directory_iterator(input_directory)) {
-            if (entry.is_regular_file()) {
-                Mat img = imread(entry.path().string(), IMREAD_GRAYSCALE);
-                if (img.empty()) {
-                    cerr << "Warning: Could not read image: " << entry.path() << endl;
-                    continue;
-                }
-                input_images.push_back({ entry.path().filename().string(), img });
-                cout << "Loaded: " << entry.path() << endl;
-            }
-        }
-    }
-    catch (const fs::filesystem_error& e) {
-        cerr << "Filesystem error: " << e.what() << endl;
-        return -1;
-    }
-
-    if (input_images.empty()) {
-        cerr << "Error: No images found in directory." << endl;
-        return -1;
-    }
-
-
-
-    cout << "Successfully loaded " << input_images.size() << " images." << endl;
-
-
-
-    // === Decide Number Of Frames To Be Processed === //
-    int nFrames_selected = 0;
-    cout << "How Many Frames would you like to process? (Min: 1, Max: " << input_images.size() << "): ";
-	cin >> nFrames_selected;
-
-    if (nFrames_selected <= 0 || nFrames_selected > input_images.size())
-    {
-		cerr << "Error: Invalid number of frames selected. Exiting." << endl;
-        return -1;
-    }
-    // === Decide Number Of Frames To Be Processed === //
-
-  
-    // === Extract Images for Processing ===
-    vector<Mat> images_only;
-
-    try {
-        for (int i = 0; i < nFrames_selected; i++) {
-            images_only.push_back(input_images[i].second);
-        }
-    }
-    catch (const std::exception& e) {
-        cerr << "Error: " << e.what() << endl;
-        return -1;
-    }
-
-    /*
-    for (const auto& pair : input_images) {
-        images_only.push_back(pair.second);
-    }
-    */
-
-    // === Validate All Images Have the Same Size ===
-    if (!validate_images_same_size(images_only)) {
-        cerr << "Error: Images are not the same size. Cannot continue." << endl;
-        return -1;
-    }
-
-    // === Compute Background Image Manually ===
-    Mat background = compute_estimated_background(images_only);
-    if (background.empty()) {
-        cerr << "Background computation failed!" << endl;
-        return -1;
-    }
-
-
-
-    // === Save and Display the Estimated Background ===
-    imwrite(output_directory + "\\estimated_background.jpg", background);
-    imshow("Estimated Background", background);
-    waitKey(500); // Show for a short time
-
-    // === Compute and Save Foreground Masks Manually ===
+void save_foreground(const vector<Mat>& images_only, const string& output_directory, const Mat& background){
     int threshold_value = 59;
     int counter = 0;
 
@@ -353,6 +322,57 @@ int main() {
     cout << "All foreground masks saved into " << output_directory << "!" << endl;
 
     system(("start \"\" \"" + output_directory + "\"").c_str());
+}
+
+int main() {
+
+    // === Receiving Directories === //
+    string input_directory, output_directory;
+    if(set_directories(input_directory, output_directory) == 0)
+        return -1;
+
+
+    vector<pair<string, Mat>> input_images;
+    // === Loading Images ===
+    if(load_images(input_images, input_directory) == 0)
+        return -1;
+    else
+        cout << "Successfully loaded " << input_images.size() << " images." << endl;
+
+
+    // === Decide Number Of Frames To Be Processed === //
+    int nFrames_selected = 0;
+    if(set_frames(nFrames_selected, input_images.size()) == 0)
+        return -1;
+  
+
+    // === Extract Images for Processing ===
+    vector<Mat> images_only;
+    if(extract_images_for_processing(images_only, input_images, nFrames_selected) == 0)
+        return -1;
+
+
+    // === Validate All Images Have the Same Size ===
+    if (!validate_images_same_size(images_only)) {
+        cerr << "Error: Images are not the same size. Cannot continue." << endl;
+        return -1;
+    }
+
+
+    // === Compute Background Image Manually ===
+    Mat background = compute_estimated_background(images_only);
+    if (background.empty()) {
+        cerr << "Background computation failed!" << endl;
+        return -1;
+    }
+
+
+    // === Save and Display the Estimated Background ===
+    save_display_background(output_directory, background);
+
+
+    // === Compute and Save Foreground Masks Manually ===
+    save_foreground(images_only, output_directory, background);
 
     return 0;
 }
